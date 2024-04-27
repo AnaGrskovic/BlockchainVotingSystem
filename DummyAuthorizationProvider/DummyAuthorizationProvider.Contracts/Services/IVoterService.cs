472264ae3013005670eb37d49cbd513ffbbe8ce1
AnaGrskovic <ana.grskovic56@gmail.com>
2024-04-27 15:11:47 +0200

    Create contracts

---
+++ DummyAuthorizationProvider/DummyAuthorizationProvider.Contracts/Services/IVoterService.cs
@@ -0,0 +1,7 @@
+﻿namespace DummyAuthorizationProvider.Contracts.Services;
+
+public interface IVoterService
+{
+    Task<string> GetToken(string oib);
+    Task<bool> IsTokenValid(string token);
+}
---
+++ DummyAuthorizationProvider/DummyAuthorizationProvider.Contracts/UoW/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using DummyAuthorizationProvider.Contracts.Entities;
+using DummyAuthorizationProvider.Contracts.Repositories;
+
+namespace DummyAuthorizationProvider.Contracts.UoW;
+
+public interface IUnitOfWork
+{
+    IRepository<Voter> Voters { get; }
+
+    Task SaveChangesAsync();
+}
