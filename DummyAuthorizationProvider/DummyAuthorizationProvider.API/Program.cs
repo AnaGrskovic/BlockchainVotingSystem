b87b19fb276588f3c3e1f86ed994fe375d8374cf
AnaGrskovic <ana.grskovic56@gmail.com>
2024-04-27 15:52:53 +0200

    Implement program.cs

--- DummyAuthorizationProvider/DummyAuthorizationProvider.API/Program.cs
+++ DummyAuthorizationProvider/DummyAuthorizationProvider.API/Program.cs
@@ -1,24 +1,41 @@
+using DummyAuthorizationProvider.Contracts.Services;
+using DummyAuthorizationProvider.Contracts.UoW;
+using DummyAuthorizationProvider.Data.Db.Context;
+using DummyAuthorizationProvider.Data.Db.UoW;
+using DummyAuthorizationProvider.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("VoterDb"),
+        opt => opt.MigrationsAssembly("DummyAuthorizationProvider.Data.Db"));
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IVoterService, VoterService>();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
 
 app.MapControllers();
 
