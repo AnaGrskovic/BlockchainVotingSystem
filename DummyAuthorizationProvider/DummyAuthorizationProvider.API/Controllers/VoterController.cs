e04e01139a61bed9f8f3cd12c9dfcc5d69b453d0
AnaGrskovic <ana.grskovic56@gmail.com>
2024-04-27 17:26:49 +0200

    Make auth provider acces token from header instead of body

--- DummyAuthorizationProvider/DummyAuthorizationProvider.API/Controllers/VoterController.cs
+++ DummyAuthorizationProvider/DummyAuthorizationProvider.API/Controllers/VoterController.cs
@@ -29,8 +29,13 @@ public class VoterController : ControllerBase
     }
 
     [HttpPost("check-token", Name = "CheckToken")]
-    public async Task<IActionResult> CheckTokenAsync([FromBody] string token)
+    public async Task<IActionResult> CheckTokenAsync()
     {
+        string? token = Request.Headers["Authorization"];
+        if (token == null)
+        {
+            return Unauthorized();
+        }
         bool isValid = await _voterService.IsTokenValidAsync(token);
         if (isValid)
         {
