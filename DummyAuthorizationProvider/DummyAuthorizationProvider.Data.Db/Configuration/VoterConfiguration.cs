e89182d00452b99cea5c2901d1a456865d0d6c40
AnaGrskovic <ana.grskovic56@gmail.com>
2024-04-27 15:18:16 +0200

    Create database related classes

---
+++ DummyAuthorizationProvider/DummyAuthorizationProvider.Data.Db/Configuration/VoterConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DummyAuthorizationProvider.Contracts.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DummyAuthorizationProvider.Data.Db.Configuration;
+
+public class VoterConfiguration : IEntityTypeConfiguration<Voter>
+{
+    public void Configure(EntityTypeBuilder<Voter> builder)
+    {
+        builder.ToTable("Voters");
+
+        builder.HasKey(v => v.Id);
+
+        builder.HasIndex(v => new { v.Oib })
+            .IsUnique();
+    }
+}
