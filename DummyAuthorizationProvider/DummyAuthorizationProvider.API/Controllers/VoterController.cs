ab0ed0960a6e1acae01a63c9cfc4779fd2448219
AnaGrskovic <ana.grskovic56@gmail.com>
2024-04-27 15:49:13 +0200

    Implement voter controller

---
+++ DummyAuthorizationProvider/DummyAuthorizationProvider.API/Controllers/VoterController.cs
@@ -0,0 +1,36 @@
+﻿using DummyAuthorizationProvider.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DummyAuthorizationProvider.API.Controllers;
+
+[ApiController]
+[Route("api/voters")]
+public class VoterController : ControllerBase
+{
+    private readonly IVoterService _voterService;
+
+    public VoterController(IVoterService voterService)
+    {
+        _voterService = voterService;
+    }
+
+    [HttpPost(Name = "GetToken")]
+    public IActionResult GetToken([FromBody] string oib)
+    {
+        return Ok(_voterService.GetToken(oib));
+    }
+
+    [HttpPost(Name = "CheckToken")]
+    public async Task<IActionResult> CheckTokenAsync([FromBody] string token)
+    {
+        bool isValid = await _voterService.IsTokenValidAsync(token);
+        if (isValid)
+        {
+            return Ok();
+        }
+        else
+        {
+            return Forbid();
+        }
+    }
+}
