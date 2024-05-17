1e3ecbbdcccd6da8b37196775a97a052de6b8456
AnaGrskovic <ana.grskovic56@gmail.com>
2024-05-17 16:58:17 +0200

    Allow requests on server, fix payload

--- voting-app-front/src/app/app.module.ts
+++ voting-app-front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideClientHydration()
--- voting-app-front/src/app/login/login.component.ts
+++ voting-app-front/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +10,27 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   personalIdNum: string = '';
 
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+
   onLogin() {
     if (this.personalIdNum) {
-      console.log('Login successful with ID:', this.personalIdNum);
+      const url = 'https://localhost:44378/api/authorization/get-token';
+      const headers = new HttpHeaders().set('Content-Type', 'application/json');
+      const payload = "\"" + this.personalIdNum + "\"";
+
+      this.http.post<number>(url, payload, { headers }).subscribe({
+        next: (response) => {
+          console.log('Login successful, token received:', response);
+          localStorage.setItem('token', response.toString());
+        },
+        error: (error) => {
+          console.error('Error during login:', error);
+          this.snackBar.open('Login failed.', 'Close', { duration: 3000 });
+        }
+      });
     } else {
       console.error('Personal identification number is required');
+      this.snackBar.open('Personal identification number is required', 'Close', { duration: 3000 });
     }
   }
 }
