b6132c2700f52dd91931b78531025d3e1a620f83
AnaGrskovic <ana.grskovic56@gmail.com>
2024-05-17 16:38:35 +0200

    Add validation

--- voting-app-front/src/app/login/login.component.ts
+++ voting-app-front/src/app/login/login.component.ts
@@ -3,17 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   personalIdNum: string = '';
 
   onLogin() {
     if (this.personalIdNum) {
-      console.log('Login successful with personal identification number:', this.personalIdNum);
+      console.log('Login successful with ID:', this.personalIdNum);
     } else {
       console.error('Personal identification number is required');
     }
   }
 }
-
