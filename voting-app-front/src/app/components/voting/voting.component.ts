7c62eadc90731e6cb325db1f8dfa57aa91db56b0
AnaGrskovic <ana.grskovic56@gmail.com>
2024-05-22 23:29:18 +0200

    Add a loading spinner to voting component

--- voting-app-front/src/app/components/voting/voting.component.ts
+++ voting-app-front/src/app/components/voting/voting.component.ts
@@ -13,6 +13,7 @@ export class VotingComponent implements OnInit {
   token: string = '';
   candidates: string[] = [];
   selectedCandidate: string = '';
+  isLoading: boolean = false;
 
   constructor(private http: HttpClient, private localStorageService: LocalStorageService, private snackBar: MatSnackBar, private router: Router) { }
 
@@ -22,42 +23,49 @@ export class VotingComponent implements OnInit {
   }
 
   fetchCandidates() {
+    this.isLoading = true;
     const url = 'https://localhost:44328/api/candidates';
     this.http.get<string[]>(url).subscribe({
       next: (response) => {
         this.candidates = response;
+        this.isLoading = false;
       },
       error: (error) => {
         console.error('Error fetching candidates:', error);
         this.snackBar.open('Error fetching candidates.', 'Close', {
           duration: 5000
         });
+        this.isLoading = false;
       }
     });
   }
 
   vote() {
-    const url = 'https://localhost:44328/api/votes';
-    const headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', this.token);
-    const payload = "\"" + this.selectedCandidate + "\"";
-    if (this.selectedCandidate) {
-      this.http.post<any>(url, payload, { headers }).subscribe({
-        next: (response) => {
-          console.log('Vote submitted successfully:', response);
-          this.router.navigate(['/results']);
-        },
-        error: (error) => {
-          console.error('Error submitting vote:', error);
-          this.snackBar.open('Error submitting vote.', 'Close', {
-            duration: 5000
-          });
-        }
-      });
-    } else {
+    if (!this.selectedCandidate) {
       console.error('No candidate selected');
       this.snackBar.open('No candidate selected.', 'Close', {
         duration: 5000
       });
+      return;
     }
+
+    this.isLoading = true;
+    const url = 'https://localhost:44328/api/votes';
+    const headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', this.token);
+    const payload = "\"" + this.selectedCandidate + "\"";
+    this.http.post<any>(url, payload, { headers }).subscribe({
+      next: (response) => {
+        console.log('Vote submitted successfully:', response);
+        this.router.navigate(['/results']);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error submitting vote:', error);
+        this.snackBar.open('Error submitting vote.', 'Close', {
+          duration: 5000
+        });
+        this.isLoading = false;
+      }
+    });
   }
 }
