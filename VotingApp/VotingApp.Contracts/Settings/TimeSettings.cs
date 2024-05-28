6ffdab3b7f61105b47ea9dbb95392e77a0a3b03b
AnaGrskovic <ana.grskovic56@gmail.com>
2024-05-28 18:50:17 +0200

    Implement time service

---
+++ VotingApp/VotingApp.Contracts/Settings/TimeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace VotingApp.Contracts.Settings;
+
+public class TimeSettings
+{
+    public const string Section = "Time";
+
+    public DateTime VotingEndTime { get; set; } = default!;
+    public DateTime CalculationEndTime { get; set; } = default!;
+}
---
+++ VotingApp/VotingApp.Data/TimeService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using VotingApp.Contracts.Services;
+using VotingApp.Contracts.Settings;
+
+namespace VotingApp.Services;
+
+public class TimeService : ITimeService
+{
+    private readonly TimeSettings _settings;
+
+    public TimeService(IOptions<TimeSettings> settings)
+    {
+        _settings = settings.Value;
+    }
+    public bool IsBlockChainCalculationTime()
+    {
+        var currentDateTime = DateTime.UtcNow;
+        return _settings.BlockChainCaluldationStartTime < currentDateTime && currentDateTime < _settings.BlockChainCaluldationEndTime;
+    }
+}
